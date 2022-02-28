676b7386644b2e85cae4982c48dce8876f376e39
Bugadinho <miguelriechi_windows8@outlook.com>
2022-02-27 22:03:51 -0300

    Rewrote the entire main file

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-import sys
+import os
 import time
+import argparse
 from PIL import Image
 from pixelfix import pixelFix
 
-if len(sys.argv) <= 1:
-    sys.exit("A file was expected!")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Takes all fully transparent pixels in a PNG and sets their colors to the nearest non-transparent pixel\'s color without altering their alpha value.')
+    parser.add_argument('path', nargs='+', help='Path of the files')
+    parser.add_argument('-t', '--threshold', type=int,
+                        default=0,
+                        choices=range(0, 255),
+                        metavar="[0-254]",
+                        help='maximum alpha value to fix (default: 0)')
+    args = parser.parse_args()
 
-print("Beginning processing...")
-
-for file in sys.argv[1:]:
-    startTime = time.time()
-    image = Image.open(file)
-    newImage = pixelFix(image)
-    newImage.save(file)
-    endTime = time.time()
-    print("Done processing " + file + ". Took " + str(endTime - startTime) + " seconds!")
+    for path in args.path:
+        print('Processing \'{}\'...'.format(path), end=' ', flush=True)
+        full_path = os.path.join(os.getcwd(), path)
+        if os.path.isfile(full_path):
+            startTime = time.time()
+            try:
+                image = Image.open(full_path)
+                try:
+                    newImage = pixelFix(image, args.threshold)
+                    try:
+                        newImage.save(full_path)
+                        endTime = time.time()
+                        print('{:.2f}s'.format(endTime - startTime))
+                    except:
+                        print('Error while saving image!')
+                except:
+                    print('Error while fixing image!')
+            except:
+                print('Error while opening image!')
+        else:
+            print('Invalid file!')
